cc1a43b901f8f745112512bc3a17aa90eddfea1a
tungzx10r <tungzx10r@gmail.com>
2026-09-22 16:56:05 +0700

    ata

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -37,7 +37,7 @@ interface AdminProfile {
 // --------------------------------------------------------------------------
 // ⚙️ การตั้งค่าระบบ
 // --------------------------------------------------------------------------
-const GAS_URL = process.env.NEXT_PUBLIC_GAS_URL || "https://script.google.com/macros/s/AKfycbzFHi_3QeBp2qMBBwZVjGBEwiqHkieMYK2YsbhU0NKG0Hs3BUaCap_4ouF-Ze9UEv3smw/exec";
+const GAS_URL = process.env.NEXT_PUBLIC_GAS_URL || "https://script.google.com/macros/s/AKfycbwLXeY1D2zaenzxNK-rJrDr-li3Lkrk6rmNhNY-8UBSu_6c_BtFzoS2_A3jyO7IvyrU/exec";
 const LIFF_ADMIN_ID = process.env.NEXT_PUBLIC_LIFF_ADMIN_ID || "2011648763-lxIG7crp"; 
 
 // 🔑 รายชื่อ UID ของ LINE ที่ระบุว่าเป็นแอดมิน
